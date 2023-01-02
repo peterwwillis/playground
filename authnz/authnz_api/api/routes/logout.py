2ab917afb7fd4985718c91c2af55dfd5d6db5613
Peter Willis <31324861+peterwwillis@users.noreply.github.com>
2023-01-02 14:03:59 -0500

    Logout works

--- authnz/authnz_api/api/routes/logout.py
+++ authnz/authnz_api/api/routes/logout.py
@@ -1,12 +1,12 @@
 
 from flask import Blueprint, redirect, url_for
-from flask_login import login_required
+from flask_login import login_required, logout_user
 
 from . import bp
 
-@bp.route('/logout')
+@bp.route('/logout', methods=["POST"])
 @login_required
 def AuthNZ_Logout():
     logout_user()
-    return redirect(url_for("index"))
+    return redirect(url_for("routes.AuthNZ_Home"))
 
